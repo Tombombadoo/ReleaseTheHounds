bd297b5c6bc20253f3f4a04f0a10b430ce2ff6d1
Tombombadoo <120600698+Tombombadoo@users.noreply.github.com>
2025-02-27 13:37:06 +0000

    Update api.py
    Corrected boolean

--- lib/api.py
+++ lib/api.py
@@ -388,8 +388,8 @@ class Client(object):
         Purge all BHCE Data
         '''
         bhpurge_body = json.dumps({
-            "deleteCollectedGraphData": true,
-            "deleteDataQualityHistory": true
+            "deleteCollectedGraphData": True,
+            "deleteDataQualityHistory": True
         }).encode('utf-8')  # Convert to JSON string and encode to bytes
         print('[*] Purging data ... ', end='')
         r = self._request('POST', '/api/v2/clear-database', body=bhpurge_body)
